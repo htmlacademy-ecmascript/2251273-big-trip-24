fa5be6194e07e6852160bddd7c65f3cb277b335e
echenko <echenko.v.g@yandex.ru>
2024-08-28 23:59:40 +0300

    +

--- src/presenter/main-presenter.js
+++ src/presenter/main-presenter.js
@@ -1,15 +1,28 @@
 import SortView from '../view/sort-view';
+import TripEventsListView from '../view/trip-events-list-view';
+import TripEventsItemView from '../view/trip-events-item-view';
 
 import {render} from '../render';
 
+const COUNT_TRIP_EVENTS = 3;
+
 class MainPresenter {
+
+  tripEventsListComponent = new TripEventsListView();
 
   constructor({container}) {
     this.container = container;
   }
 
   init() {
+    // отрисовка сортировки
     render(new SortView(), this.container);
+
+    // отрисовка списка событий
+    render(this.tripEventsListComponent, this.container);
+    for (let i = 0; i < COUNT_TRIP_EVENTS; i++) {
+      render(new TripEventsItemView(), this.tripEventsListComponent.getElement());
+    }
   }
 }
 
---
+++ src/view/trip-events-list-view.js
@@ -0,0 +1,26 @@
+import { createElement } from '../render.js';
+
+function createTripEventsList() {
+  return `<ul class="trip-events__list">
+          </ul>`;
+}
+
+class TripEventsListView {
+  getTemplate() {
+    return createTripEventsList();
+  }
+
+  getElement() {
+    if (!this.element) {
+      this.element = createElement(this.getTemplate());
+    }
+
+    return this.element;
+  }
+
+  removeElement() {
+    this.element = null;
+  }
+}
+
+export default TripEventsListView;
