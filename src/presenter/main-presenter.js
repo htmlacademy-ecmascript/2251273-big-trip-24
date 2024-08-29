e93b327b7e032c5553a9fcc622759dfe4d68898f
echenko <echenko.v.g@yandex.ru>
2024-08-29 08:33:39 +0300

    +

--- src/presenter/main-presenter.js
+++ src/presenter/main-presenter.js
@@ -1,13 +1,14 @@
 import SortView from '../view/sort-view';
 import TripEventsListView from '../view/trip-events-list-view';
 import TripEventsItemView from '../view/trip-events-item-view';
+import EditEventFormView from '../view/edit-form-view';
 
 import {render} from '../render';
 
 const COUNT_TRIP_EVENTS = 3;
 
 class MainPresenter {
-
+  // список событий (компонент)
   tripEventsListComponent = new TripEventsListView();
 
   constructor({container}) {
@@ -19,7 +20,14 @@ class MainPresenter {
     render(new SortView(), this.container);
 
     // отрисовка списка событий
+    // добавление в компонет списка событий элемемта
     render(this.tripEventsListComponent, this.container);
+
+    // отрисовка формы редактирования
+    // добавление в компонет списка событий элемемта
+    render(new EditEventFormView(), this.tripEventsListComponent.getElement());
+
+
     for (let i = 0; i < COUNT_TRIP_EVENTS; i++) {
       render(new TripEventsItemView(), this.tripEventsListComponent.getElement());
     }
